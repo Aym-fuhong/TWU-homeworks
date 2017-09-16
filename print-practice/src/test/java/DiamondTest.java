20355749dd10f824af1e310f8ef259aafb75a54d
fuhong-thoughtworks <hfu@thoughtworks.com>
2017-09-16 16:10:53 +0800

    add diamond test case

--- print-practice/src/test/java/DiamondTest.java
+++ print-practice/src/test/java/DiamondTest.java
@@ -7,22 +7,62 @@ public class DiamondTest {
     private Diamond diamond = new Diamond();
 
     @Test
-    public void center_triangle_test() {
+    public void center_triangle_test_case_one() {
         assertThat(diamond.centered_triangle(1), is("*\n"));
     }
 
     @Test
-    public void inverted_triangle_test() {
+    public void center_triangle_test_case_two() {
+        assertThat(diamond.centered_triangle(2), is(" *\n***\n"));
+    }
+
+    @Test
+    public void center_triangle_test_case_three() {
+        assertThat(diamond.centered_triangle(3), is("  *\n ***\n*****\n"));
+    }
+
+    @Test
+    public void inverted_triangle_test_case_one() {
         assertThat(diamond.inverted_triangle(1), is(""));
     }
 
     @Test
-    public void center_diamond_test() {
+    public void inverted_triangle_test_case_two() {
+        assertThat(diamond.inverted_triangle(2), is(" *\n"));
+    }
+
+    @Test
+    public void inverted_triangle_test_case_three() {
+        assertThat(diamond.inverted_triangle(3), is(" ***\n  *\n"));
+    }
+
+    @Test
+    public void center_diamond_test_case_one() {
         assertThat(diamond.centered_diamond(1), is("*\n"));
     }
 
     @Test
-    public void center_diamond_name_test() {
+    public void center_diamond_test_case_two() {
+        assertThat(diamond.centered_diamond(2), is(" *\n***\n *\n"));
+    }
+
+    @Test
+    public void center_diamond_test_case_three() {
+        assertThat(diamond.centered_diamond(3), is("  *\n ***\n*****\n ***\n  *\n"));
+    }
+
+    @Test
+    public void center_diamond_name_test_case_one() {
         assertThat(diamond.centered_diamond_name(1), is("Aym\n"));
+    }
+
+    @Test
+    public void center_diamond_name_test_case_two() {
+        assertThat(diamond.centered_diamond_name(2), is(" *\nAym\n *\n"));
+    }
+
+    @Test
+    public void center_diamond_name_test_case_three() {
+        assertThat(diamond.centered_diamond_name(3), is("  *\n ***\nAym\n ***\n  *\n"));
     }
 }
