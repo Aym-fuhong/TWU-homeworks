38ffe30fda0a4c3408a3c74a1645b3feb90aa6a3
fuhong-thoughtworks <hfu@thoughtworks.com>
2017-09-16 16:12:50 +0800

    add factor test case

--- print-practice/src/test/java/FactorTest.java
+++ print-practice/src/test/java/FactorTest.java
@@ -8,7 +8,17 @@ public class FactorTest {
     private Factor factor = new Factor();
 
     @Test
-    public void generate_test() {
+    public void generate_test_case_one() {
         assertThat(factor.generate(30), is("[2, 3, 5]"));
+    }
+
+    @Test
+    public void generate_test_case_two() {
+        assertThat(factor.generate(1), is("[]"));
+    }
+
+    @Test
+    public void generate_test_case_three() {
+        assertThat(factor.generate(10), is("[2, 5]"));
     }
 }
